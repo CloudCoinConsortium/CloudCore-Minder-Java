6f4b528c7ac679a16b9aa31ecce534ea8f4f7ca4
gaoguangli <jin10211@hotmail.com>
2018-11-28 11:18:42 -0800

    construct the project

---
+++ src/com/cloudcore/minder/core/Config.java
@@ -0,0 +1,24 @@
+package com.cloudcore.minder.core;
+
+public class Config {
+    /* Constant Fields */
+
+    public static final String TAG_BACKUP = "CloudCoin Backup ";
+
+    public static final String TAG_GALLERY = "Gallery";
+    public static final String TAG_BACKUPER = "Backupper";
+    public static final String TAG_BANK = "Bank";
+    public static final String TAG_FRACKED = "Fracked";
+
+    // name of file which will be placed in command folder
+    public static String TAG_file_name = "backupper";
+    
+    public static final String TAG_CLOUD_COIN = "CloudCoin";
+    public static final String TAG_COMMAND = "Command";
+    public static final String TAG_BACKUP_DEFAULT = "Backup";
+    public static final String TAG_LOGS = "Logs";
+    
+    
+    public static final String TAG_ACCOUNTS = "Accounts";
+    public static final String TAG_PASSWORDS = "Passwords";
+}
---
+++ src/com/cloudcore/minder/utils/FileUtils.java
@@ -0,0 +1,123 @@
+package com.cloudcore.minder.utils;
+
+import com.cloudcore.minder.core.Config;
+import com.cloudcore.minder.core.FileSystem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FileUtils {
+ /* Methods */
+    
+    /**
+     * Returns an array containing all filenames in a directory.
+     *
+     * @param folderPath the folder to check for files.
+     * @return String Array.
+     */
+    public static String getCommandFileContnet(String folderPath) {
+        File folder = new File(folderPath);
+        String commandFile = "";
+        if (folder.isDirectory()) {
+
+            File[] filenames = folder.listFiles();
+
+            if (null != filenames) {
+                for (File file : filenames) {
+                    if (file.isFile()) {
+                        if (file.getName().contains(Config.TAG_file_name)) {
+                            commandFile = readCommandFile(file.getAbsolutePath());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return commandFile;
+    }
+
+    /**
+     * Returns an array containing all filenames in a directory.
+     *
+     * @param accountName the account from passwords folder.
+     * @return String Array.
+     */
+    public static String getAccountFileName(String accountName) {
+        File folder = new File(FileSystem.AccountFolder);
+        String commandFile = "";
+        if (folder.isDirectory()) {
+
+            File[] filenames = folder.listFiles();
+
+            if (null != filenames) {
+                for (File file : filenames) {
+                    if (file.isFile()) {
+                        if (file.getName().contains(accountName)) {
+                            commandFile = file.getAbsolutePath();
+                            commandFile = fetchAccountContent(commandFile);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return commandFile;
+    }
+
+    public static String readCommandFile(String filePath) {
+
+        InputStream is;
+        try {
+            is = new FileInputStream(filePath);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+            String line = buf.readLine();
+            StringBuilder sb = new StringBuilder();
+
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = buf.readLine();
+            }
+           
+            return sb.toString();
+//            String fileAsString = sb.toString();
+//            JSONObject jObj =new JSONObject(fileAsString);
+//            if(jObj.has("toPath")){
+//                return jObj.getString("toPath");
+//            }
+        } catch (IOException | JSONException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+    
+    public static String fetchAccountContent(String filePath) {
+
+        InputStream is;
+        try {
+            is = new FileInputStream(filePath);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+            String line = buf.readLine();
+            StringBuilder sb = new StringBuilder();
+
+            while (line != null) {
+                sb.append(line).append("\n");
+                line = buf.readLine();
+            }
+           
+            return sb.toString();
+        } catch (IOException | JSONException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+}
